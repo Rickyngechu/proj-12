6d063b91f46fbec815af515f3d5e9a9af7f2dc0f
Rickyngechu <ekngechu003@gmail.com>
2023-10-28 19:56:07 +0300

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "Bright-Red": "hsl(12, 88%, 59%)",
+      "Dark-Blue": "hsl(228, 39%, 23%)",
+
+      "Dark-Grayish-Blue": "hsl(227, 12%, 61%)",
+      "Very-Dark-Blue": "hsl(233, 12%, 13%)",
+      "Very-Pale-Red": "hsl(13, 100%, 96%)",
+      "Vary-Light-Gray": "hsl(0, 0%, 98%)",
+    },
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
