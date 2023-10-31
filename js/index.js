08e0bd3dcf8b2ecbccf6f302283abe0dcef3e14e
Rickyngechu <ekngechu003@gmail.com>
2023-10-31 19:29:37 +0300

    Swiper updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,112 @@
 "use strict";
+
+const sliderCont = document.querySelector(".reviews-cont");
+const slides = document.querySelectorAll(".slider");
+const dotContainer = document.querySelector(".dots");
+let curSlide = 0;
+let maxSlide = slides.length;
+// sliderCont.style.overflow = "visible";
+// sliderCont.style.maxWidth = "100rem";
+console.log(slides);
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+createDots();
+const activeDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach(dot => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+activeDot(0);
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+  });
+};
+goToSlide(0);
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+
+let touchStartX = null;
+let touchEndX = null;
+
+document.addEventListener("touchstart", e => {
+  touchStartX = e.touches[0].clientX;
+});
+
+document.addEventListener("touchend", e => {
+  touchEndX = e.changedTouches[0].clientX;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  if (touchStartX !== null && touchEndX !== null) {
+    const swipeDistance = touchEndX - touchStartX;
+    if (swipeDistance > 50) {
+      // A swipe to the right has occurred
+      //   console.log("Swipe right detected");
+      prevSlide();
+    }
+
+    // Reset the values for the next swipe
+    touchStartX = null;
+    touchEndX = null;
+  }
+}
+
+let touchStartX2 = null;
+let touchEndX2 = null;
+
+document.addEventListener("touchstart", e => {
+  touchStartX2 = e.touches[0].clientX;
+});
+
+document.addEventListener("touchend", e => {
+  touchEndX2 = e.changedTouches[0].clientX;
+  handleSwipe2();
+});
+
+function handleSwipe2() {
+  if (touchStartX2 !== null && touchEndX2 !== null) {
+    const swipeDistance = touchStartX2 - touchEndX2;
+    if (swipeDistance > 50) {
+      // A swipe to the left has occurred
+      //   console.log("Swipe left detected");
+      nextSlide();
+    }
+
+    // Reset the values for the next swipe
+    touchStartX2 = null;
+    touchEndX2 = null;
+  }
+}
